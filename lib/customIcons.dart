729a6602bfda0ae612435a790c2029921d7de7dc
Jet Xu <jet.xu@ccconceptcn.com>
2020-01-03 17:55:32 +0800

    利用pageView制作滚动图

---
+++ lib/customIcons.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static const IconData menu = IconData(
+      // menu为图标的名称
+      0xe624, // 代表的是该图标的代码
+      fontFamily: "CustomIcons" // 在pubspec.yaml中设置的字体名称，要对应上
+      );
+  static const IconData search = IconData(
+      // menu为图标的名称
+      0xece1, // 代表的是该图标的代码
+      fontFamily: "CustomIcons" // 在pubspec.yaml中设置的字体名称，要对应上
+      );
+  static const IconData option = IconData(
+      // option为图标的名称
+      0xe6d5, // 代表的是该图标的代码
+      fontFamily: "CustomIcons" // 在pubspec.yaml中设置的字体名称，要对应上
+      );
+}
+
+// class CustomIcons {
+//   static const IconData menu = IconData(0xe900, fontFamily: "CustomIcons");
+//   static const IconData option = IconData(0xe902, fontFamily: "CustomIcons");
+// }
---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "assets/imgs/image_04.jpg",
+  "assets/imgs/image_03.jpg",
+  "assets/imgs/image_02.jpg",
+  "assets/imgs/image_01.png",
+];
+
+List<String> title = [
+  "Hounted Ground",
+  "Fallen In Love",
+  "The Dreaming Moon",
+  "Jack the Persian and the Black Catel",
+];
